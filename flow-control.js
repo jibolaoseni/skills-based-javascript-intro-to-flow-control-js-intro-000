d4679c0ef4cc758c93c9e4569228fa22ae5029ed
Ajibola Oseni <auo3@student.london.ac.uk>
2020-05-13 10:21:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,27 @@
 function basicTeenager(age) {
-
+  if (age === 13 && age <= 20)
+  return 'You are a teenager!'
 }
 
 function teenager(age) {
+  if (age > 12 && age< 20){
+   return 'You are a teenager!'
+  } else {
+    return "You are not a teenager"
+  }
 
 }
 
 function ageChecker(age) {
-
+  if (age > 12 && age< 20){
+  return 'You are a teenager!'
+  }
+  if else (age < 13) { # I believe you want } else if { here?
+    return 'You are a kid'
+  }
+  if else (age > 19) { # And here?
+    return 'You are a grownup'
+  }
 }
 
 function ternaryTeenager(age) {
